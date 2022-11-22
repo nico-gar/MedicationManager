ee22cf7fdcf81367a3d22adbb5928f3b0e2d4491
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-11-22 12:04:26 -0700

    added custom views and view controllers

---
+++ MedicationManager/CustomViews/MedicationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MedicationTableViewCell.swift
+//  MedicationManager
+//
+//  Created by Nicolas Garaycochea on 11/22/22.
+//
+
+import UIKit
+
+class MedicationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MedicationManager/ViewControllers/MedicationDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MedicationDetailViewController.swift
+//  MedicationManager
+//
+//  Created by Nicolas Garaycochea on 11/22/22.
+//
+
+import UIKit
+
+class MedicationDetailViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
